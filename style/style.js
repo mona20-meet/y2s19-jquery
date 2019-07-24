ab3935b68bbae113361fa3ebfcfa755d95ddd747
myname21-meet <myname21@meet.mit.edu>
2019-07-24 07:17:39 -0400

    yeye

--- style/style.js
+++ style/style.js
@@ -3,16 +3,17 @@
 
 
 function updateSize(newSize) {
-  // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
+  $('#logo').css('width',newSize);
 }
 
 
 function updateLeftPosition(newX) {
-	// FINISH ME
+	$('#logo').css('transform','rotateX('+newX+'deg)');
 
 }
 
 function updateTopPosition(newY) {
+	$('#logo').css('transform',"rotateY("+newY+'deg)');
 	// FINISH ME
 }
 
